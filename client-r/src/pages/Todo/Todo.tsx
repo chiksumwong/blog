bcb1c787ed76b27d92e3e99e3dbebdbe21fad496
CHIKSUM WONG <chiksum.wong@gmail.com>
2021-03-24 23:35:46 +0800

    File Structure Refactor

---
+++ client-r/src/pages/Todo/Todo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Todo.css';
+
+const Todo: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Space</h1>
+        </div>
+    );
+};
+
+export default Todo;
