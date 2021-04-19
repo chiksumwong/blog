676d95327ba4070e64186c3efdcc3470b500d8e1
chiksumwong <chiksum.wong@gmail.com>
2021-04-19 19:03:17 +0800

    Update Counter.tsx

--- client-r/src/pages/TicTacToe/Counter.tsx
+++ client-r/src/pages/TicTacToe/Counter.tsx
@@ -5,13 +5,22 @@ React Hoot
 2. useContext()   
 3. useReducer()
 4. useEffect()
+5. or customize your own hoot
 
 */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Counter(): JSX.Element {
+    // init to 0
     const [count, setCount] = useState(0);
+    //const [todos, setTodos] = useState([{ text: 'First Todo Item' }]);
+
+    // same as componentDidMount and componentDidUpdate
+    useEffect(() => {
+        document.title = `You clicked ${count} times.`;
+        console.log('logging use effect');
+    });
 
     return (
         <div>
